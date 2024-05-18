6b351ed4f5e77d8241cf5c55b25e2e7efbb77e36
euotaa <164763111+euotaa@users.noreply.github.com>
2024-05-18 23:19:08 +0700

    Add files via upload

---
+++ storage/framework/views/88d811b1473afb1c789bf21061f3d5a310518501.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('layouts.default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\HP\Documents\1- Kuliah\4 - Semester 4\Pemrograman Framework\Modul8 lanjutan\resources\views/home.blade.php ENDPATH**/ ?>
